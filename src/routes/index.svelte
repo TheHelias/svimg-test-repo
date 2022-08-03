8f3862f545f9ae12ef53afcb225e4e4caafe2335
ThElias <akande.tolu@yahoo.com>
2022-08-03 15:28:53 +0100

    create test app

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Image from 'svimg';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Image src="favicon.png" />
---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import { imagePreprocessor } from 'svimg';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess(),
+		imagePreprocessor({
+			inputDir: 'static',
+			outputDir: 'static/g',
+			webp: true,
+			avif: true
+		})
+	],
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
